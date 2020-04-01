168a48e2f53b284c12083b173608419d69bedf98
axp172130 <anindita.palit98@gmail.com>
2020-04-01 15:53:18 -0500

    Errors APalit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_155259) do
+ActiveRecord::Schema.define(version: 2020_04_01_201653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_155259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
